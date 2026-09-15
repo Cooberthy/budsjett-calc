83787c94d92c3501e3c5e9042d37774432ea5d2f
Cooberthy <lebea012@osloskolen.no>
2026-09-15 10:23:26 +0200

    show total income

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let pay = document.getElementById("salary").value
+let extra = document.getElementById("other").value
+let income = 0
 
 function funk(){
     pay = document.getElementById("salary").value
-    document.getElementById("showfunds").textContent = pay
+    extra = document.getElementById("other").value
+    income = parseInt(extra) + parseInt(pay)
+    document.getElementById("showfunds").textContent = "total income: " + income
 }
