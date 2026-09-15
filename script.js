efec1c6c386e5d45c6147f5df106ce9e4196be51
Cooberthy <lebea012@osloskolen.no>
2026-09-15 10:24:32 +0200

    change onclikc name

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let pay = document.getElementById("salary").value
 let extra = document.getElementById("other").value
 let income = 0
 
-function funk(){
+function showincome(){
     pay = document.getElementById("salary").value
     extra = document.getElementById("other").value
     income = parseInt(extra) + parseInt(pay)
